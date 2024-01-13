d3bdf3b664a4bf0e23a217a612c9d32ecb5966ba
Ivan Polyanin <meloman639@gmail.com>
2024-01-13 12:50:37 +0300

    dobavlena func pe4ati

--- life.go
+++ life.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+   
+func field(array [][]int){
+    for i := 0; i < len(array);i++{
+	fmt.Print("\n")
+	for j := 0; j < len(array); j++{
+	    if array[i][j] == 0{
+		fmt.Print(" ")
+	    } else {
+	    fmt.Print("*")
+	    }
+	}
+    }
+}
 
 func main() {
-    
+    array := [][]int{
+    {0, 1, 0},
+    {0, 0, 1},
+    {1, 1, 1},
+    }
+field(array)
 }
