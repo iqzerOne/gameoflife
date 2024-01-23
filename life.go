fc364c743e7f73496d0b8d5531b5f53b810903e5
Ivan Polyanin <polyaninbackend@gmail.com>
2024-01-23 20:11:55 +0300

    dobavlena func proverki kletki na liveordead

--- life.go
+++ life.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Поле
 func field(array [][]int) {
 	for i := 0; i < len(array); i++ {
 		fmt.Print("\n")
@@ -15,6 +16,7 @@ func field(array [][]int) {
 	}
 }
 
+// подсчет соседей
 func cell_counter(array [][]int, y, x int) int {
 	res := 0
 	res += predel(array, y, x+1)
@@ -27,6 +29,35 @@ func cell_counter(array [][]int, y, x int) int {
 	res += predel(array, y+1, x+1)
 	return res
 }
+
+// Копирования двумерного массива
+func duplicate(array [][]int) [][]int {
+	duplicate := make([][]int, len(array))
+	for i := range array {
+		duplicate[i] = make([]int, len(array[i]))
+		copy(duplicate[i], array[i])
+	}
+	return duplicate
+}
+
+// Проверка клетки на живую или мертвую
+func livedead(array [][]int) [][]int {
+	new_array := duplicate(array)
+	for y := 0; y < len(array); y++ {
+		for x := 0; x < len(array); x++ {
+			counter := cell_counter(array, y, x)
+			if (counter > 3 || counter < 2) && array[y][x] == 1 {
+				new_array[y][x] = 0
+			}
+			if counter == 3 && array[y][x] == 0 {
+				new_array[y][x] = 1
+			}
+		}
+	}
+	return new_array
+}
+
+// проверка выхода за предел массива
 func predel(array [][]int, y, x int) int {
 	if (y >= 0 && x >= 0) && (y < len(array) && x < len(array)) {
 		if array[y][x] == 1 {
@@ -38,9 +69,10 @@ func predel(array [][]int, y, x int) int {
 func main() {
 	array := [][]int{
 		{0, 1, 0},
-		{0, 0, 1},
-		{1, 1, 1},
+		{0, 1, 0},
+		{0, 1, 0},
 	}
 	field(array)
-	fmt.Print("\n", cell_counter(array, 2, 1))
+	// array = livedead(array)
+	// field(array)
 }
